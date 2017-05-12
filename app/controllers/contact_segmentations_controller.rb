f327f484910de85b6abbe78e1a89d6893a64a222
Thiago Charão <charao.thiago@gmail.com>
2017-05-12 17:02:46 -0300

    style: apply bootstrap to contact segmentation

--- app/controllers/contact_segmentations_controller.rb
+++ app/controllers/contact_segmentations_controller.rb
@@ -1,34 +1,26 @@
 class ContactSegmentationsController < ApplicationController
   before_action :set_contact_segmentation, only: [:show, :edit, :update, :destroy]
 
-  # GET /contact_segmentations
-  # GET /contact_segmentations.json
   def index
     @contact_segmentations = ContactSegmentation.all
   end
 
-  # GET /contact_segmentations/1
-  # GET /contact_segmentations/1.json
   def show
   end
 
-  # GET /contact_segmentations/new
   def new
     @contact_segmentation = ContactSegmentation.new
   end
 
-  # GET /contact_segmentations/1/edit
   def edit
   end
 
-  # POST /contact_segmentations
-  # POST /contact_segmentations.json
   def create
     @contact_segmentation = ContactSegmentation.new(contact_segmentation_params)
 
     respond_to do |format|
       if @contact_segmentation.save
-        format.html { redirect_to @contact_segmentation, notice: 'Contact segmentation was successfully created.' }
+        format.html { redirect_to action: "index", notice: 'Contact segmentation was successfully created.' }
         format.json { render :show, status: :created, location: @contact_segmentation }
       else
         format.html { render :new }
@@ -37,8 +29,6 @@ class ContactSegmentationsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /contact_segmentations/1
-  # PATCH/PUT /contact_segmentations/1.json
   def update
     respond_to do |format|
       if @contact_segmentation.update(contact_segmentation_params)
@@ -51,8 +41,6 @@ class ContactSegmentationsController < ApplicationController
     end
   end
 
-  # DELETE /contact_segmentations/1
-  # DELETE /contact_segmentations/1.json
   def destroy
     @contact_segmentation.destroy
     respond_to do |format|
