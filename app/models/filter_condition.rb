48e83d179d3e9b7ab64dbb9128780ef0b187edcf
Thiago Charão <charao.thiago@gmail.com>
2017-05-14 14:25:44 -0300

    feat: validate fields and comparators on API and create selects on interface

--- app/models/filter_condition.rb
+++ app/models/filter_condition.rb
@@ -1,6 +1,23 @@
 class FilterCondition < ApplicationRecord
+  COMPARATORS = {"equals to" => "=",
+                 "contains" => "%%",
+                 "starts with" => "=*",
+                 "ends with" => "*=",
+                 "greater than" => ">",
+                 "less than" => "<",
+                 "greater than or equals to" => ">=",
+                 "less than or equals to" => "<="}
   belongs_to :contact_segmentation, required: true, inverse_of: :filter_conditions
-  validates :field, presence: true
-  validates :comparator, presence: true
+  validates :field,
+            presence: true,
+            inclusion: {
+              in: Contact.comparable_fields.keys,
+              message: "%{value} is not a valid field, possible values are #{Contact.comparable_fields.map{|k,v| "'#{k}'"}.join(', ')}" }
+  validates :comparator,
+            presence: true,
+            inclusion: {
+              in: COMPARATORS.values,
+              message: "%{value} is not a valid comparator, possible values are #{COMPARATORS.map{|k,v| "'#{v}'"}.join(', ')}" }
   validates :value, presence: true
+
 end
