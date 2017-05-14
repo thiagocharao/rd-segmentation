6e68d56a10448f934488114dc62a9407e439d412
Thiago Charão <charao.thiago@gmail.com>
2017-05-14 16:06:17 -0300

    feat: adjusting comparator way to be shown

--- app/models/filter_condition.rb
+++ app/models/filter_condition.rb
@@ -1,12 +1,12 @@
 class FilterCondition < ApplicationRecord
-  COMPARATORS = {"equals to" => "=",
-                 "contains" => "%%",
-                 "starts with" => "=*",
-                 "ends with" => "*=",
-                 "greater than" => ">",
-                 "less than" => "<",
-                 "greater than or equals to" => ">=",
-                 "less than or equals to" => "<="}
+  COMPARATORS = {"=" => "equals to",
+                 "%%" => "contains",
+                 "=*" => "starts with",
+                 "*=" => "ends with",
+                 ">" => "greater than",
+                 "<" => "less than",
+                 ">=" => "greater than or equals to",
+                 "<=" => "less than or equals to"}
   belongs_to :contact_segmentation, required: true, inverse_of: :filter_conditions
   validates :field,
             presence: true,
@@ -16,8 +16,8 @@ class FilterCondition < ApplicationRecord
   validates :comparator,
             presence: true,
             inclusion: {
-              in: COMPARATORS.values,
-              message: "%{value} is not a valid comparator, possible values are #{COMPARATORS.map{|k,v| "'#{v}'"}.join(', ')}" }
+              in: COMPARATORS.keys,
+              message: "%{value} is not a valid comparator, possible values are #{COMPARATORS.map{|k,v| "'#{k}'"}.join(', ')}" }
   validates :value, presence: true
 
 end
